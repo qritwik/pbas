62c2642d27aff00fa573cd5467c9df0c2c8af017
Aayush Nandkeolyar <nandkeolyar.aayush@gmail.com>
2018-10-26 00:00:57 +0530

    Update settings.py

--- facform/settings.py
+++ facform/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
+
+
 """
 
 import os
