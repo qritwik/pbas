f5f8cd2456dea24fced46b0b10dd0cd7914e42dc
Tarun Jain <tarunjofficial@gmail.com>
2019-01-03 23:58:33 +0530

    filefield size increased

--- facform1/formatChecker.py
+++ facform1/formatChecker.py
@@ -17,7 +17,6 @@ class ContentTypeRestrictedFileField(FileField):
             250MB - 214958080
     """
     def __init__(self, *args, **kwargs):
-        self.content_types = kwargs.pop("content_types")
         self.max_upload_size = kwargs.pop("max_upload_size")
 
         super(ContentTypeRestrictedFileField, self).__init__(*args, **kwargs)
@@ -27,12 +26,9 @@ class ContentTypeRestrictedFileField(FileField):
 
         file = data.file
         try:
-            content_type = file.content_type
-            if content_type in self.content_types:
-                if file._size > self.max_upload_size:
-                    raise forms.ValidationError(_('Please keep filesize under %s. Current filesize %s') % (filesizeformat(self.max_upload_size), filesizeformat(file._size)))
-            else:
-                raise forms.ValidationError(_('Filetype not supported.'))
+            if file._size > self.max_upload_size:
+                raise forms.ValidationError(_('Please keep filesize under %s. Current filesize %s') % (filesizeformat(self.max_upload_size), filesizeformat(file._size)))
+        
         except AttributeError:
             pass
 
