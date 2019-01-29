9c962afd96cd8c67ef5fd678150a9891ab08e665
Tarun Jain <tarunjofficial@gmail.com>
2019-01-29 18:53:19 +0530

    assistant preview improved

--- facform1/views.py
+++ facform1/views.py
@@ -22,30 +22,30 @@ from django.core.mail import send_mail, EmailMessage
 
 
 
-def email_doc(self, name):
-	qs = User.objects.get(username=name)
-	url = 'https://pbas.bmsit.ac.in/ao_teacher1_display/' + name
+# def email_doc(self, name):
+# 	qs = User.objects.get(username=name)
+# 	url = 'https://pbas.bmsit.ac.in/ao_teacher1_display/' + name
 
 	
-	# DRIVER = 'chromedriver'
-	# driver = webdriver.Chrome(DRIVER)
-	# driver.get(url)
-	# scn = name + '.png'
-	# # screenshot = driver.get_screenshot_as_png()
-	# screenshot = driver.save_screenshot(scn)
-	# driver.quit()
+# 	# DRIVER = 'chromedriver'
+# 	# driver = webdriver.Chrome(DRIVER)
+# 	# driver.get(url)
+# 	# scn = name + '.png'
+# 	# # screenshot = driver.get_screenshot_as_png()
+# 	# screenshot = driver.save_screenshot(scn)
+# 	# driver.quit()
 
 
-	# pdf = pdfkit.from_url(url, False)
+# 	# pdf = pdfkit.from_url(url, False)
 
-	email = EmailMessage(
-				'PBAS',
-				'Hi, ' + qs.first_name + '\n\n' +'Your PBAS report is attached below: \n\nThanks,\nAO-BMSIT' ,
-				'PBAS Support <ao@bmsit.in>',
-				[qs.email],
-				)
-	email.attach_file(screenshot)
-	email.send()	
+# 	email = EmailMessage(
+# 				'PBAS',
+# 				'Hi, ' + qs.first_name + '\n\n' +'Your PBAS report is attached below: \n\nThanks,\nAO-BMSIT' ,
+# 				'PBAS Support <ao@bmsit.in>',
+# 				[qs.email],
+# 				)
+# 	email.attach_file(screenshot)
+# 	email.send()	
 
 
 
@@ -1503,9 +1503,9 @@ def ao_teacher_display(request,name):
 
 
 
-	if request.method == 'POST':
-		email_doc(name)
-		return HttpResponseRedirect('/ao_display'+ data1.department)
+	# if request.method == 'POST':
+	# 	email_doc(name)
+	# 	return HttpResponseRedirect('/ao_display'+ data1.department)
 
 	return render(request,'ao_teacher_display.html',context=context1)
 
