a23be8dc993af80cdb0f300bcb0fbf328e32efe3
Ravi Kumar Singh <ravikrsngh.rks@gmail.com>
2019-09-03 15:32:27 +0530

    abc

--- facform1/views.py
+++ facform1/views.py
@@ -767,10 +767,10 @@ def login(request):
 			email = user.email
 			phone = user.phone
 			random_otp = r''.join(random.choice('0123456789') for i in range(4))
-			#phone_otp(random_otp,phone)
+			phone_otp(random_otp,phone)
 
-			#email_otp(random_otp,user.email,user.first_name)
-			random_otp="1234"
+			email_otp(random_otp,user.email,user.first_name)
+			#random_otp="1234"
 			hashed_pwd = make_password(random_otp)
 			User.objects.filter(username=username).update(password=hashed_pwd)
 
